1ea179112c7d2273fa163f4b9e45b72792f7cea6
wJexson <timur1704200311@gmail.com>
2022-12-16 10:25:43 +0300

    Практическая работа 22

--- src/ru/mirea/task22/Task22.java
+++ src/ru/mirea/task22/Task22.java
@@ -1,4 +1,16 @@
 package ru.mirea.task22;
 
+class Client  {
+    public void Sit(Chair chair){
+        System.out.println("Клиент сел на " + chair.getName());
+    }
+}
+
 public class Task22 {
+    public static void main(String[] args) {
+        ChairFactory chairFactory = new ChairFactory();
+        Chair chair = chairFactory.createChair(Chairs.MAGIC_CHAIR);
+        Client client = new Client();
+        client.Sit(chair);
+    }
 }
